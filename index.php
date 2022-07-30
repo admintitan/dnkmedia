6dc558c50661b35a74ed369251d4f186104d14fe
admintitan <aconcerntitan@gmail.com>
2022-07-30 18:17:22 +0300

    dnk

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <title>dnk media</title>
+    <link rel="icon" href="media/icondnk.png" type="image/icon type">
+</head>
+<body>
+    <section class="showcase">
+        <video src="media/video1.mp4" muted loop autoplay></video>
+        <img class="logodnk" src="media/logodnk.png">
+        <a href="https://dnk.video/"><img class="logo logo1" src="media/peertube.png"></a>
+        <a href="https://www.youtube.com/channel/UCaKc3MJii0LhM2K5sPKJ8wA"><img class="logo logo2" src="media/ydnk.png"></a>
+        <a href="https://www.youtube.com/channel/UCuLHK3s3tD37UNcG3yF2wrQ"><img class="logo logo3" src="media/ydnku.png"></a>
+        <a href="https://fs.dnk.video/"><img class="logo logo4" src="media/logofs.png"></a>
+        <a href="https://dnkforum.com/"><img class="logo logo5" src="media/logodnkforum.png"></a>
+        <a href=""><img class="logo6" src="media/logofacebook.png"></a>
+    </section>
+</body>
+</html>
